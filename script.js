1fe2595e7b7f38cbda9e312ec57e78902e7205b2
murugalaskhm8-art <murugalaskhm8@gmail.com>
2026-05-18 12:07:45 +0530

    Add bar chart implementation using Chart.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+const ctx = document.getElementById('chart');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr'],
+        datasets: [{
+            label: 'Sales',
+            data: [50000, 70000, 60000, 90000]
+        }]
+    }
+});
